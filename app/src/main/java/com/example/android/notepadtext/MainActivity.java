f24c7bc6f68091310bb75aa32d5fc1bd8c35dc4f
unknown <719330034@qq.com>
2018-12-28 19:05:22 +0800

    “修改”

--- app/src/main/java/com/example/android/notepadtext/MainActivity.java
+++ app/src/main/java/com/example/android/notepadtext/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.android.notepadtext;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,7 +17,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar!=null){
+            actionBar.hide();
+        }
+
+        Button edit = findViewById(R.id.goEdit);
+        edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(MainActivity.this, EditActivity.class );
+                startActivity(intent1);
+            }
+        });
+
+        Button delete = findViewById(R.id.goDelete);
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent2 = new Intent(MainActivity.this, DeleteActivity.class);
+                startActivity(intent2);
+            }
+        });
     }
 }
